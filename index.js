9044024bc028dca660bbb7d1d6e157b645cf4e7b
Thinushi <thinupavani@gmail.com>
2022-03-16 23:23:10 +0530

    network error fixed

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import productRoutes from "./routes/products.js";
 
 const app = express();
 
-app.use("/products", productRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/products", productRoutes);
 
 //mongo db set up
 const CONNECTION_URL =
