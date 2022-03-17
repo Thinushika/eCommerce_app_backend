f5f51526798776e2f8a3ff5b61a8df5a3d81f8ec
Thinushi <thinupavani@gmail.com>
2022-03-17 13:46:31 +0530

    env file

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import productRoutes from "./routes/products.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,12 +16,13 @@ app.use(cors());
 app.use("/products", productRoutes);
 
 //mongo db set up
-const CONNECTION_URL =
-  "mongodb+srv://thinushika:F8J9ivSbeJ1zD0J0@cluster0.sxyft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 3000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
   )
